77a1a7545abcbf2b3ea1d90ccf4637c6664e28ea
Nathaniel Meyers <nate.meyers@ibm.com@Nathaniels-MBP.lan>
2021-10-23 11:52:16 -0500

    re-introduce forward prop test

---
+++ test/src/forwardPropTest.cpp
@@ -0,0 +1,21 @@
+// C++ file defining the forward propogation test
+
+#include "forwardPropTest.h"
+
+int main(int argc, char* argv[])
+{       
+	int numLayers = 4;
+	int shape[4] = {42*3, 42*3*3, 42*3*3, 1};
+	NeuralNetwork nn(numLayers, shape, &sigmoid);
+
+	float output;
+	float inputs[42*3];
+	for(int i=0; i<42*3; i++)
+	{
+		inputs[i] = ((float)rand() / RAND_MAX) - ((float)rand() / RAND_MAX);
+		std::cout << inputs[i] << std::endl;
+	}
+	nn.forwardProp(inputs, &output);
+	std::cout << "output: " << output << std::endl;
+}
+
